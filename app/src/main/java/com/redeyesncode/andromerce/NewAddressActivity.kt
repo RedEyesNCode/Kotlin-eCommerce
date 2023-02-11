b854dadaeb787b9d9e905be6ead27624405819ae
RedEyesNCode <ashutosh.singh19071999@gmail.com>
2023-02-11 16:28:14 +0530

    Update for address activity and product detail

---
+++ app/src/main/java/com/redeyesncode/andromerce/NewAddressActivity.kt
@@ -0,0 +1,11 @@
+package com.redeyesncode.andromerce
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewAddressActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_address)
+    }
+}
